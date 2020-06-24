6317d5f507b9ef71e19351726f87d76df2ea5603
Yonipineda <yonipineda1010@icloud.com>
2020-06-23 21:28:40 -0400

    last update for the day.

--- 2-Player/game.py
+++ 2-Player/game.py
@@ -15,4 +15,26 @@ pygame.display.set_icon(pygame.image.load("assets/Grid_Icon.png"))
 pygame.event.set_allowed(None) # Tells pygame not to check for any inputs
 
 # Allowed events 
-allowed_events = "Not Yet Implemented"
+allowed_events = (pygame.MOUSEMOTION, pygame.MOUSEBUTTONDOWN, pygame.KEYDOWN, pygame.KEYUP,
+                  pygame.QUIT)
+
+for event in allowed_events:
+    pygame.event.set_allowed(event)
+
+# initialize Simulation
+Simulation = utils.Simulation()
+SimulationBoard = board.SimulationBoard(Simulation)
+SimulationBoard.set_up(Simulation.set_up_chances)
+
+# Initialize Game 
+Game = None 
+GameBoard = None 
+GameBoard.set_up(Game.set_up_chances)
+
+# Initialize Help Menu 
+Help = utils.Help()
+
+# Initialize Main Menu 
+Menu = utils.MainMenu()
+MenuChoice = Menu.get_choice(Screen)
+
