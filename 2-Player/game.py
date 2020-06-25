62db75873dff65cf82cba8536141d466a8913311
Yonipineda <yonipineda1010@icloud.com>
2020-06-25 01:04:53 -0400

    couple errors to fix.

--- 2-Player/game.py
+++ 2-Player/game.py
@@ -11,7 +11,7 @@ pygame.init()
 
 # Initialize screen display 
 Screen = pygame.display.set_mode((1080, 650))
-pygame.display.set_icon(pygame.image.load("assets/Grid_Icon.png"))
+pygame.display.set_icon(pygame.image.load("grid.png"))
 pygame.event.set_allowed(None) # Tells pygame not to check for any inputs
 
 # Allowed events 
@@ -27,9 +27,9 @@ SimulationBoard = board.SimulationBoard(Simulation)
 SimulationBoard.set_up(Simulation.set_up_chances)
 
 # Initialize Game 
-Game = None 
-GameBoard = None 
-GameBoard.set_up(Game.set_up_chances)
+Game = utils.Game() 
+GameBoard = board.GameBoard(Game, players=True)
+GameBoard.set_up(Game.set_up_chances, rotational_symmetry=Game.num_of_players)
 
 # Initialize Help Menu 
 Help = utils.Help()
@@ -37,4 +37,20 @@ Help = utils.Help()
 # Initialize Main Menu 
 Menu = utils.MainMenu()
 MenuChoice = Menu.get_choice(Screen)
+
+
+# while game is running
+while MenuChoice in (Menu.buttons[:3]): # if non-quit button was pressed.
+    if MenuChoice == Menu.buttons[0]: # simulator button
+        Simulation.run(Screen, SimulationBoard)
+    elif MenuChoice == Menu.buttons[1]: # game button 
+        if Game.run(Screen, GameBoard): # run the game
+            Game = utils.Game() 
+            GameBoard = board.GameBoard(Game, players=True) # reset to play again
+            GameBoard.set_up(Game.set_up_chances, rotational_symmetry=Game.num_of_players)
+    elif MenuChoice == Menu.buttons[2]: # Help button
+        Help.display(Screen)
+    MenuChoice = Menu.get_choice(Screen) # another choice is requested when menu is returned
+
+set_up.quit_game()
 
